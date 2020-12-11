e867a803a5ca0b3e7839b90baf2076eae55c29ab
kedwards413 <kaela.rose.edwards@gmail.com>
2020-12-11 19:50:16 +0000

    updates

--- src/main.js
+++ src/main.js
@@ -4,6 +4,32 @@ import "./style.css";
 
 import "./assets/img/4geeks.ico";
 
+let who = ["The dog", "My grandma", "His turtle", "My bird"];
+let action = ["ate", "destroyed", "crushed", "broke"];
+let what = ["my homework", "the keys", "the car"];
+let when = [
+  "before the class",
+  "right on time",
+  "when I finished",
+  "during my lunch",
+  "while I was praying"
+];
+
+function excuseGenerator(arr) {
+  let random = Math.floor(Math.random() * arr.length);
+  return arr[random];
+}
+var kaela =
+  excuseGenerator(who) +
+  " " +
+  excuseGenerator(action) +
+  " " +
+  excuseGenerator(what) +
+  " " +
+  excuseGenerator(when);
+
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  console.log(kaela);
 };
+
+document.write(kaela);
